9cfcf00f7aa4e251aa41d9851d79985a8ecc85d3
ryip94 <ryip94@yahoo.com>
2023-08-27 03:40:03 -0700

    Fixed image sizing. Completed Skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -35,44 +35,40 @@ const Skills = () => {
   }, []);
   return (
     <Container className="text-white mt-5">
-      <h2>My Skills</h2>
-      <Container className="image-container">
-        <Row className="gx-4 gy-4">
-          <Col className="img-wrapper">
-            <Image src={html} />
-            <div className="image-label">HTML</div>
-          </Col>
-          <Col className="img-wrapper">
-            <Image src={css} />
-            <div className="image-label">CSS</div>
-          </Col>
-          <Col className="img-wrapper">
-            <Image src={js} />
-            <div className="image-label">JavaScript</div>
-          </Col>
-          <Col className="img-wrapper">
-            <Image src={react} />
-            <div className="image-label">React</div>
-          </Col>
-        </Row>
-        <Row className="gx-4 gy-4">
-          <Col className="img-wrapper">
-            <Image src={java} />
-            <div className="image-label">Java</div>
-          </Col>
-          <Col className="img-wrapper">
-            <Image src={python} />
-            <div className="image-label">Python</div>
-          </Col>
-          <Col className="img-wrapper">
-            <Image src={cpp} />
-            <div className="image-label">C++</div>
-          </Col>
-          <Col className="img-wrapper">
-            <Image src={bootstrap} />
-            <div className="image-label">Bootstrap</div>
-          </Col>
-        </Row>
+      <h2 className="mb-5">My Skills</h2>
+      <Container className="flex-container">
+        <div className="flex-item">
+          <Image src={html} />
+          <div className="image-label">HTML</div>
+        </div>
+        <div className="flex-item">
+          <Image src={css} />
+          <div className="image-label">CSS</div>
+        </div>
+        <div className="flex-item">
+          <Image src={js} />
+          <div className="image-label">JavaScript</div>
+        </div>
+        <div className="flex-item">
+          <Image src={react} />
+          <div className="image-label">React</div>
+        </div>
+        <div className="flex-item">
+          <Image src={java} />
+          <div className="image-label">Java</div>
+        </div>
+        <div className="flex-item">
+          <Image src={python} />
+          <div className="image-label">Python</div>
+        </div>
+        <div className="flex-item">
+          <Image src={cpp} />
+          <div className="image-label">C++</div>
+        </div>
+        <div className="flex-item">
+          <Image src={bootstrap} />
+          <div className="image-label">Bootstrap</div>
+        </div>
       </Container>
 
       <div
