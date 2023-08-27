e124442a7e4403b935cf1e44b04c011d28be660b
ryip94 <ryip94@yahoo.com>
2023-08-27 02:15:55 -0700

    Added scrollbar with underline

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Container from "react-bootstrap/Container";
 import NavMain from "./components/NavMain";
 import About from "./components/About";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <Container>
       <NavMain />
       <About />
+      <Skills />
     </Container>
   );
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const Skills = () => {
+  return (
+    <Container className="text-white mt-5">
+      <h2>My Skills</h2>
+    </Container>
+  );
+};
+
+export default Skills;
