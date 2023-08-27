038c67e111f4f0e2ad565e17c84fd6a0437d882b
ryip94 <ryip94@yahoo.com>
2023-08-26 23:51:58 -0700

    Link hover animations

--- src/components/NavMain.jsx
+++ src/components/NavMain.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import icon from "../assets/coding.png";
-
+import Nav from "react-bootstrap/Nav";
+import "./NavMain.css";
 function NavMain() {
   return (
     <>
-      <Navbar>
+      <Navbar collapseOnSelect expand="lg" variant="dark">
         <Container>
           <Navbar.Brand
             href="#home"
@@ -22,11 +23,29 @@ function NavMain() {
               height="26"
               className="d-inline-block align-top"
             />
-            <span style={{ marginLeft: 18, fontSize: 16, color: "white" }}>
+            <span
+              className="text-white"
+              style={{ marginLeft: 18, fontSize: 16 }}
+            >
               Ryan Yip | Software Engineer
             </span>
           </Navbar.Brand>
         </Container>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#About" className="text-white ">
+              About
+            </Nav.Link>
+            <Nav.Link href="#Projects" className="text-white mx-4">
+              Projects
+            </Nav.Link>
+            <Nav.Link href="#Contact" className="text-white">
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
