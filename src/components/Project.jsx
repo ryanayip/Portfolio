a06535c779f6547a668ad8e252785adb040030e3
ryip94 <ryip94@yahoo.com>
2023-08-28 16:11:45 -0700

    Fixed box shadows and img alignment

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,30 +7,75 @@ import rpg from "../assets/projects/rpgpic.png";
 
 const Project = () => {
   return (
-    <Container className="text-white project-cont">
-      <Container className="cont-left">
-        <h2 className="mb-4">Project Title</h2>
-        <ul className="techItems">
-          <li className="techUsed">React</li>
-          <li className="techUsed">JavaScript</li>
-          <li className="techUsed">HTML</li>
-          <li className="techUsed">CSS</li>
-          <li className="techUsed">Firebase</li>
-          <li className="techUsed">Netlify</li>
-        </ul>
-        <Button
-          size="lg"
-          style={{ backgroundColor: "#FF6A6A", border: "none", width: 200 }}
-        >
-          View Project
-        </Button>
-        <p>
-          This project is being described in this paragraph. It's such a cool
-          project. I used a buynch of stuff. It can do cool stuff
-        </p>
+    <Container className="main-cont">
+      <Container className="text-white project-cont">
+        <Container className="cont-left">
+          <h2 className="mb-4">Project Title</h2>
+          <ul className="techItems">
+            <li className="techUsed">React</li>
+            <li className="techUsed">JavaScript</li>
+            <li className="techUsed">HTML</li>
+            <li className="techUsed">CSS</li>
+            <li className="techUsed">Firebase</li>
+            <li className="techUsed">Netlify</li>
+          </ul>
+          <Button size="lg" className="view-btn">
+            View Project
+          </Button>
+          <p>
+            This project is being described in this paragraph. It's such a cool
+            project. I used a buynch of stuff. It can do cool stuff
+          </p>
+        </Container>
+        <Container className="cont-right">
+          <Image src={rpg} className="project-pic" />
+        </Container>
       </Container>
-      <Container className="cont-right">
-        <Image src={rpg} rounded fluid />
+      <Container className="text-white project-cont">
+        <Container className="cont-left">
+          <h2 className="mb-4">Project Title</h2>
+          <ul className="techItems">
+            <li className="techUsed">React</li>
+            <li className="techUsed">JavaScript</li>
+            <li className="techUsed">HTML</li>
+            <li className="techUsed">CSS</li>
+            <li className="techUsed">Firebase</li>
+            <li className="techUsed">Netlify</li>
+          </ul>
+          <Button size="lg" className="view-btn">
+            View Project
+          </Button>
+          <p>
+            This project is being described in this paragraph. It's such a cool
+            project. I used a buynch of stuff. It can do cool stuff
+          </p>
+        </Container>
+        <Container className="cont-right">
+          <Image src={rpg} className="project-pic" />
+        </Container>
+      </Container>
+      <Container className="text-white project-cont">
+        <Container className="cont-left">
+          <h2 className="mb-4">Project Title</h2>
+          <ul className="techItems">
+            <li className="techUsed">React</li>
+            <li className="techUsed">JavaScript</li>
+            <li className="techUsed">HTML</li>
+            <li className="techUsed">CSS</li>
+            <li className="techUsed">Firebase</li>
+            <li className="techUsed">Netlify</li>
+          </ul>
+          <Button size="lg" className="view-btn">
+            View Project
+          </Button>
+          <p>
+            This project is being described in this paragraph. It's such a cool
+            project. I used a buynch of stuff. It can do cool stuff
+          </p>
+        </Container>
+        <Container className="cont-right">
+          <Image src={rpg} className="project-pic" />
+        </Container>
       </Container>
     </Container>
   );
