2a9b0dfb39d0dd1dd1546baef40bf80e973c23d5
ryip94 <ryip94@yahoo.com>
2023-08-28 18:46:36 -0700

    Added grocery pal to projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button";
 import "./Project.css";
 import Image from "react-bootstrap/Image";
 import rpg from "../assets/projects/rpgpic.png";
+import grocery from "../assets/projects/grocerypal.png";
 
 const Project = () => {
   return (
@@ -16,7 +17,6 @@ const Project = () => {
             <li className="techUsed">JavaScript</li>
             <li className="techUsed">HTML</li>
             <li className="techUsed">CSS</li>
-            <li className="techUsed">Firebase</li>
             <li className="techUsed">Netlify</li>
           </ul>
           <Button size="lg" className="view-btn">
@@ -35,26 +35,39 @@ const Project = () => {
       </Container>
       <Container className="text-white project-cont">
         <Container className="cont-left">
-          <h2 className="mb-4">Project Title</h2>
+          <h2 className="mb-4">Grocery Pal</h2>
           <ul className="techItems">
-            <li className="techUsed">React</li>
             <li className="techUsed">JavaScript</li>
             <li className="techUsed">HTML</li>
             <li className="techUsed">CSS</li>
             <li className="techUsed">Firebase</li>
             <li className="techUsed">Netlify</li>
           </ul>
-          <Button size="lg" className="view-btn">
+          <Button
+            size="lg"
+            className="view-btn"
+            href="https://yipsgrocerypal.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            as="a"
+          >
             View Project
           </Button>
           <p>
-            This project is being described in this paragraph. It's such a cool
-            project. I used a buynch of stuff. It can do cool stuff
+            A grocery app that allows users to create a grocery list, add items,
+            and check them off as they shop. The app is built using Vanilla
+            JavaScript, HTML, and Vanilla CSS. The data is stored in Firebase
+            and deployed on Netlify. The currently link brings you to a version
+            than implements local storage instead of Firebase.
           </p>
         </Container>
         <Container className="cont-right">
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <Image src={rpg} className="project-pic" />
+          <a
+            href="https://yipsgrocerypal.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src={grocery} className="project-pic" />
           </a>{" "}
         </Container>
       </Container>
